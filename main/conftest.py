917ff8f3b46da9098adf961433c6ae941cc6e7a2
ping2alexander <ping2alexander@gmail.com>
2022-06-25 15:04:43 +0530

    updated run_onlscript.sh file and added conftest.py

--- main/conftest.py
+++ main/conftest.py
@@ -1,11 +1,26 @@
+import pytest
+
 def pytest_addoption(parser):
-    parser.addoption("--filename", action="store", default="default name")
+    parser.addoption("--filename", action="store", default="default name1")
+    parser.addoption("--testbed", action="store", default="default name2")
+    
+#@pytest.fixture
+#def filename(request):
+#    return request.config.getoption("--filename")
+
+#@pytest.fixture
+#def testbed(request):
+#    return request.config.getoption("--testbed")
 
 
 def pytest_generate_tests(metafunc):
     # This is called for every test. Only get/set command line arguments
     # if the argument is specified in the list of test "fixturenames".
-    option_value = metafunc.config.option.filename
-    if 'filename' in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize("filename", [option_value])
+    option_value1 = metafunc.config.option.filename
+    if 'filename' in metafunc.fixturenames and option_value1 is not None:
+        metafunc.parametrize("filename", [option_value1])
+
+    option_value2 = metafunc.config.option.testbed
+    if 'testbed' in metafunc.fixturenames and option_value2 is not None:
+        metafunc.parametrize("testbed", [option_value2])
 
